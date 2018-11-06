98370d5dc9e8fde39239448330dcf1546a74e391
Smith <DSmith04@lear.com>
2018-11-06 14:19:56 -0500

    Arrow Functions and Implicit Returns
    Arrow functions simplify the format of callback functions so that the
    function keyword can be excluded and curly braces around the body
    can be excluded, and the return keyword can be excluded when the
    body includes a single line that represents the return statement.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Hello TypeScript!");
+interface IPizza {
+  name: string;
+  toppings: string[];
+}
+
+const pizzas: IPizza[] = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] }
+];
+
+let mappedPizzas: string[];
+mappedPizzas = pizzas.map(function (pizza: IPizza): string {
+    return pizza.name.toUpperCase();
+  });
+
+let arrowMappedPizzas: string[];
+arrowMappedPizzas = pizzas.map((pizza => pizza.name.toUpperCase()));
+
+console.log(mappedPizzas);
+console.log(arrowMappedPizzas);
