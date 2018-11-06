a4a4d05631b0e26a04c1527bee5f35c65ba48f46
Smith <DSmith04@lear.com>
2018-11-06 14:48:10 -0500

    Default Function Parameters
    Typescript and ES6 provide a new feature that allows you to set
    default variables for function parameters.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
-interface IPizza {
-  name: string;
-  toppings: string[];
+/* Default Function Parameters */
+
+let title: string = 'Default Function Parameters';
+
+let description: string = `
+Typescript and ES6 provide a new feature that allows you to set default variables for function parameters.
+`;
+
+let code: string = `
+  function multiply (a: number, b: number = 25): number {
+    return a * b;
+  }
+
+  console.log(multiply(5));
+  console.log(multiply(5, 35));
+`;
+
+let body: any = document.getElementsByTagName('body');
+let header: string = `<h1>${ title }</h1>`;
+let desc: string = `<p>${ description}</p>`;
+let sample: string = `<pre>${ code }</pre>`;
+
+body[0].innerHTML = header + desc + sample;
+
+
+function multiply (a: number, b: number = 25): number {
+  return a * b;
 }
 
-const pizzas: IPizza[] = [
-  { name: 'Pepperoni', toppings: ['pepperoni'] }
-];
-
-let mappedPizzas: string[];
-mappedPizzas = pizzas.map(function (pizza: IPizza): string {
-    return pizza.name.toUpperCase();
-  });
-
-let arrowMappedPizzas: string[];
-arrowMappedPizzas = pizzas.map((pizza => pizza.name.toUpperCase()));
-
-console.log(mappedPizzas);
-console.log(arrowMappedPizzas);
+console.log(multiply(5));
+console.log(multiply(5, 35));
