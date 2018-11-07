f9dcabdbfec5f91acacec35482cff1ca152311e3
Danya D. Smith <dsmith04@lear.com>
2018-11-07 10:25:34 -0500

    Rest Parameters
    Rest parameters allow you to spread an array into discrete values
    so you can combine the array values with other discrete values for
    processing in a function.

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,22 @@
 /* Default Function Parameters */
 
-let title: string = 'Object Literal Improvements';
+let title: string = 'Rest Parameters';
 
 let description: string = `
-You can combine existing data types and data structures that are
-assigned to variables as properties in a new object literal using
-a shorthand syntax. There is also a shorthand syntax you can use
-when assigning a function to an object property.
+Rest parameters allow you to spread an array into discrete values
+so you can combine the array values with other values for processing
+in a function.
 `;
 
 let code: string = `
-const pizza = {
-  name: 'Pepperoni',
-  price: 15,
-  getName() {
-    return this.name;
-  }
+function sumAll(...arr: any[]): number {
+  return arr.reduce((prev: number, next: number) => prev + next);
 }
 
-const toppings = ['pepperoni'];
-
-// the old way...
-
-const oldOrder = {
-  pizza: pizza,
-  toppings: toppings
-};
-
-// the new way...
-
-const order = { pizza, toppings };
-
-function createOrder (pizza, toppings) {
-  return { pizza, toppings };
-}
-
-console.log('Old Order:', oldOrder);
-console.log('Order:', order);
-console.log('Create Order:', createOrder(pizza, toppings));
+const sum: number = sumAll(...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+const anotherSum: number = sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log('Sum:', sum);
+console.log('Another Sum:', anotherSum);
 `;
 
 let body: any = document.getElementsByTagName('body');
@@ -47,28 +26,11 @@ let sample: string = `<pre>${ code }</pre>`;
 
 body[0].innerHTML = header + desc + sample;
 
-const pizza = {
-  name: 'Pepperoni',
-  price: 15,
-  getName(): any {
-    return this.name;
-  }
+function sumAll(...arr: any[]): number {
+  return arr.reduce((prev: number, next: number) => prev + next);
 }
 
-const toppings: string[] = ['pepperoni'];
-
-// the old way...
-const oldOrder: any = {
-  pizza: pizza,
-  toppings: toppings
-};
-
-const order: any = { pizza, toppings };
-
-function createOrder (pizza: any, toppings: string[]): any {
-  return { pizza, toppings };
-}
-
-console.log('Old Order:', oldOrder);
-console.log('Order:', order);
-console.log('Create Order:', createOrder(pizza, toppings));
+const sum: number = sumAll(...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+const anotherSum: number = sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log('Sum:', sum);
+console.log('Another Sum:', anotherSum);
